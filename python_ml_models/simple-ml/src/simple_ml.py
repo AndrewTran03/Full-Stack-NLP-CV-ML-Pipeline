d59bfd4d6df534e475ad8b6ef8e82992d77546e1
Andrew Tran <andrewtranva@gmail.com>
2024-12-19 15:07:45 -0500

    Added skeleton for basic ML integration directory implementation (with 'setup.py' working)

---
+++ python_ml_models/simple-ml/src/simple_ml.py
@@ -0,0 +1,18 @@
+import pika
+
+from utils.logging import LOGGER
+
+def main() -> None:
+    print("Hello World")
+    
+    # Test logging capabilities
+    LOGGER.info("Hello World")
+    LOGGER.debug("Hello World")
+    LOGGER.warning("Hello World")
+    LOGGER.error("Hello World")
+    LOGGER.critical("Hello World")
+    
+    return None
+
+if __name__ == "__main__":
+    main()
