40e8297ed924cc590af0f7ca15d85efea3b27667
Andrew Tran <andrewtranva@gmail.com>
2024-12-19 23:02:56 -0500

    Fixed formatting on main Python script

--- python_ml_models/simple-ml/src/scripts/simple_ml.py
+++ python_ml_models/simple-ml/src/scripts/simple_ml.py
@@ -2,17 +2,19 @@ import pika
 
 from src.utils.logging import LOGGER
 
+
 def main() -> None:
     print("Hello World")
-    
+
     # Test logging capabilities
     LOGGER.info("Hello World")
     LOGGER.debug("Hello World")
     LOGGER.warning("Hello World")
     LOGGER.error("Hello World")
     LOGGER.critical("Hello World")
-    
+
     return None
+
 
 if __name__ == "__main__":
     main()
