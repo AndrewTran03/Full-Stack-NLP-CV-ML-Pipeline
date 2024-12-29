ff68179c19ddfd82826edc7393008173aceb1682
Andrew Tran <andrewtranva@gmail.com>
2024-12-28 23:59:50 -0500

    Fixed Card Component styling (Prettier) and Angular syntax highlighting

--- client-angular-nx-standalone/src/app/components/card/card.component.ts
+++ client-angular-nx-standalone/src/app/components/card/card.component.ts
@@ -4,13 +4,11 @@ import { Card, DEFAULT_CARD } from "../../shared/models/card.model";
 import { CardService } from "../../shared/services/card.service";
 import { PythonBE, DEFAULT_PYTHON_BE } from "../../shared/models/python.model";
 import { PythonService } from "../../shared/services/python.service";
-
 @Component({
   selector: "app-card",
   imports: [CommonModule],
   templateUrl: "./card.component.html",
-  styleUrl: "./card.component.css",
-  providers: [CardService]
+  styleUrl: "./card.component.css"
 })
 export class CardComponent {
   @Input() input = "";
@@ -22,7 +20,7 @@ export class CardComponent {
     private readonly pythonService: PythonService
   ) {}
 
-  btnClick(): void {
+  btnClick() {
     this.cardService.getFilledCard().subscribe((newCard: Card) => {
       // console.log({ newCard });
       this.card.set(newCard);
