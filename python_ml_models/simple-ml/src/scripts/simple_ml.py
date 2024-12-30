92a97075f0d888fd01a607fccdd5d52b2062560d
Andrew Tran <andrewtranva@gmail.com>
2024-12-29 19:01:09 -0500

    Added a skeleton for Simple ML Model Implementation (will update later with RabbitMQ implementation)

--- python_ml_models/simple-ml/src/scripts/simple_ml.py
+++ python_ml_models/simple-ml/src/scripts/simple_ml.py
@@ -1,6 +1,30 @@
+import os
+import pickle
+from typing import TypedDict
+
 import pika
+from sklearn.tree import DecisionTreeClassifier
 
 from src.utils.logging import LOGGER
+
+# CONSTANTS
+ML_MODEL_PATH = "src/models/dt_classifier_model.pkl"
+
+
+class PredictionDict(TypedDict):
+    prediction: str
+
+
+def predict() -> PredictionDict:
+    if not os.path.exists(ML_MODEL_PATH):
+        LOGGER.error("Error: Model does NOT exist")
+        return {"prediction": ""}
+
+    with open(ML_MODEL_PATH, "rb") as predict_file:
+        ml_model: DecisionTreeClassifier = pickle.load(predict_file)
+        LOGGER.debug(f"Model: {ml_model.__class__.__name__}")
+        result = "GOT HERE"
+        return {"prediction": f"Hello World - {result}"}
 
 
 def main() -> None:
@@ -12,6 +36,9 @@ def main() -> None:
     LOGGER.warning("Hello World")
     LOGGER.error("Hello World")
     LOGGER.critical("Hello World")
+
+    # Test ML model (Printing the result - for now)
+    print(predict())
 
     return None
 
