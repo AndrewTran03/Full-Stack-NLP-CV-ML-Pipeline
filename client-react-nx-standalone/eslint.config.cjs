390d8aa3be958ceddd1120a15cc4ce8ee369f6d0
Andrew Tran <andrewtranva@gmail.com>
2025-02-07 23:51:30 -0500

    Added custom ESLint Config for React Nx project

--- client-react-nx-standalone/eslint.config.cjs
+++ client-react-nx-standalone/eslint.config.cjs
@@ -1,35 +1,42 @@
-const nx = require('@nx/eslint-plugin');
-const eslintPluginUnicorn = require('eslint-plugin-unicorn');
-const globals = require('globals');
-const tseslint = require('typescript-eslint');
-const ESLINT_RULES = require('@andrewt03/eslint-typescript-rules');
+const nx = require("@nx/eslint-plugin");
+const eslintUnicornPlugin = require("eslint-plugin-unicorn");
+const globals = require("globals");
+const tseslint = require("typescript-eslint");
+const ESLINT_RULES = require("@andrewt03/eslint-typescript-rules");
+const eslintSimpleImportSortPlugin = require("eslint-plugin-simple-import-sort");
+const eslintImportPlugin = require("eslint-plugin-import");
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
+  ...nx.configs["flat/base"],
+  ...nx.configs["flat/typescript"],
+  ...nx.configs["flat/javascript"],
   {
-    ignores: ['**/dist'],
+    ignores: ["**/dist"]
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     // Override or add rules here
-    rules: {},
+    rules: {}
   },
-  ...nx.configs['flat/react'],
+  ...nx.configs["flat/react"],
   {
-    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    files: ["src/**/*.ts", "src/**/*.tsx"],
     languageOptions: {
       globals: globals.builtin,
       parser: tseslint.parser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: __dirname,
-      },
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
     plugins: {
-      unicorn: eslintPluginUnicorn,
-      '@typescript-eslint': tseslint.plugin,
+      unicorn: eslintUnicornPlugin,
+      "@typescript-eslint": tseslint.plugin,
+      "simple-import-sort": eslintSimpleImportSortPlugin,
+      import: eslintImportPlugin,
     },
     rules: {
       // Standard ESLint Rules
@@ -40,6 +47,16 @@ module.exports = [
 
       // Unicorn ESLint Rules
       ...ESLINT_RULES.UNICORN_ESLINT_CONFIG_RULES,
-    },
-  },
+
+      // ESLint Rules: Console/Debugger to "Warn"
+      ...ESLINT_RULES.CONSOLE_DEBUGGER_WARN_ESLINT_CONFIG_RULES,
+
+      // ESLint Rules: Sorting Imports
+      ...ESLINT_RULES.SORT_IMPORT_ESLINT_CONFIG_RULES,
+
+      // ESLint Rules: React
+      ...ESLINT_RULES.REACT_ESLINT_CONFIG_RULES,
+      ...ESLINT_RULES.REACT_HOOKS_ESLINT_CONFIG_RULES
+    }
+  }
 ];
